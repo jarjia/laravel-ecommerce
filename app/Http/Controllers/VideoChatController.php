dfa6260d304225f5a3f9154c985fca8b72600254
jarji <jarjaabua@gmail.com>
2024-02-14 17:13:24 +0400

    fix: fix prod

--- app/Http/Controllers/VideoChatController.php
+++ app/Http/Controllers/VideoChatController.php
@@ -14,86 +14,61 @@ class VideoChatController extends Controller
             'random' => 'required'
         ]);
 
-        $keyExists = Redis::exists("offer:{$attrs['random']}");
-
-        return response()->json(['exists' => $keyExists, 'offer' => json_decode(Redis::get("offer:{$attrs['random']}"))]);
+        return response()->json(json_decode(Redis::get("offer:{$attrs['random']}")));
     }
 
     public function createVideoSession(Request $request)
     {
         $attrs = $request->validate([
             'candidates' => 'required|array',
-            'streamID' => 'required'
+            'random' => 'required'
         ]);
 
         event(new VideoChatEvent(['offerCandidates' => $attrs['candidates']]));
 
-        Redis::set("offerCandidates:{$attrs['streamID']}", json_encode($attrs['candidates']), 'EX', 86400);
+        Redis::set("offerCandidates:{$attrs['random']}", json_encode($attrs['candidates']));
 
-        return response()->json(201);
+        return response()->json(json_decode(Redis::get("offerCandidates:{$attrs['random']}")));
     }
 
     public function offer(Request $request)
     {
         $attrs = $request->validate([
             'offer' => 'required',
-            'random' => 'required',
-            'triggerAnswer' => 'nullable'
+            'random' => 'required'
         ]);
 
         event(new VideoChatEvent(['offer' => $attrs['offer']]));
 
-        if(isset($attrs['triggerAnswer'])) {
-            $answers = json_decode(Redis::get("answer_ids:{$attrs['random']}"));
+        Redis::set("offer:{$attrs['random']}", json_encode($attrs['offer']));
 
-            foreach($answers as $answer) {
-                event(new VideoChatEvent(['triggerAnswer' => json_decode(Redis::get("answerCandidates:{$attrs['random']}:{$answer}"))]));
-            }
-        }
-
-        Redis::set("offer:{$attrs['random']}", json_encode($attrs['offer']), 'EX', 86400);
-
-        return response()->json(200);
+        return response()->json(json_decode(Redis::get("offer:{$attrs['random']}")));
     }
 
     public function createAnswer(Request $request)
     {
         $attrs = $request->validate([
             'candidates' => 'required|array',
-            'random' => 'required',
-            'answer_id' => 'required'
+            'random' => 'required'
         ]);
-
         event(new VideoChatEvent(['answerCandidates' => $attrs['candidates']]));
 
-        $keyExists = Redis::exists("answer_ids:{$attrs['random']}");
-        $answer_ids = [];
+        Redis::set("answerCandidates:{$attrs['random']}", json_encode($attrs['candidates']));
 
-        if($keyExists) {
-            $answer_ids = json_decode(Redis::get("answer_ids:{$attrs['random']}"));
-
-            array_push($answer_ids, $attrs['answer_id']);
-        } else {
-            Redis::set("answer_ids:{$attrs['random']}", json_encode([$attrs['answer_id']]), 'EX', 86400);
-        }
-
-        Redis::set("answerCandidates:{$attrs['random']}:{$attrs['answer_id']}", json_encode($attrs['candidates']), 'EX', 86400);
-
-        return response()->json(json_decode(Redis::get("answerCandidates:{$attrs['random']}:{$attrs['answer_id']}")));
+        return response()->json(json_decode(Redis::get("answerCandidates:{$attrs['random']}")));
     }
 
     public function answer(Request $request)
     {
         $attrs = $request->validate([
             'answer' => 'required',
-            'random' => 'required',
-            'answer_id' => 'required'
+            'random' => 'required'
         ]);
 
         event(new VideoChatEvent(['answer' => $attrs['answer']]));
 
-        Redis::set("answer:{$attrs['random']}:{$attrs['answer_id']}", json_encode($attrs['answer']), 'EX', 86400);
+        Redis::set("answer:{$attrs['random']}", json_encode($attrs['answer']));
 
-        return response()->json(json_decode(Redis::get("answer:{$attrs['random']}:{$attrs['answer_id']}")));
+        return response()->json(json_decode(Redis::get("answer:{$attrs['random']}")));
     }
 }
